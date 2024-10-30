5ec8179ab26b7617c98e5da72ab3a686d637d961
anson [home] <anson.zeng@student.tdsb.on.ca>
2024-10-30 18:21:52 -0400

    fixed functions except polynomial

--- script.js
+++ script.js
@@ -1,56 +1,71 @@
-function heron(){
-    //document.getElementById("heronA").innerHTML;
-    let a = 0;
-    let b = 4;
-    let c = 5;
-    if (a<=0 || b<=0  || c<=0){
-        //FIX THIS RETURN STATEMENT
-        return console.log("no work");
-    } else {
-        let area=1/4 * Math.sqrt(4*a*a*b*b-Math.pow((a*a+b*b-c*c), 2));
-        console.log (area);
-    }
+//event action listener for heron button
+document.getElementById("heronCalc").addEventListener("click", function(){
+    const sideA=document.getElementById("heronA").value;
+    const sideB=document.getElementById("heronB").value;
+    const sideC=document.getElementById("heronC").value;
+    document.getElementById("heronArea").value=heron(sideA, sideB, sideC);
+});
+
+//event action listener for ambiguous case button
+document.getElementById("ambCalc").addEventListener("click", function(){
+    const angle=document.getElementById("ambAngle").value;
+    const sideA=document.getElementById("ambSideA").value;
+    const sideB=document.getElementById("ambSideB").value;
+    document.getElementById("ambType").value=ambCase(angle, sideA, sideB);
+});
+
+//event action listener for newton button
+document.getElementById("newtonCalc").addEventListener("click", function(){
+    const guess=document.getElementById("newRoot").value;
+    document.getElementById("newApp").value=newton(guess);
+});
+
+//event action listener for polynomial button
+
+function heron(a, b, c){
+    console.log("hi");
+    const root=Math.sqrt(4*a*a*b*b-Math.pow((a*a+b*b-c*c), 2));
+    if (a>0 && b>0 && c>0){
+        if (root>0){
+            return (Math.round(root*25, 2))/100;
+        }
+    } return "No triangle";
 }
 
-function ambCase(){
-    let angle=2;
-    let a=240;
-    let b=9;
-    if (angle<=0 || a<=0  || b<=0){
-        //FIX THIS RETURN STATEMENT
-        return console.log("no work");
-    }
-    //put this in one line with a declaration
-    angle=angle*Math.PI/2
-    //move this declaration up
-    let h=b*Math.sin(angle)
-    if (a<=90){
-        if (a<h){
-            return console.log("no triangle");
-        } else if (a==h){
-            return console.log("right triangle");
-        } else if (a>b){
-            return console.log("one triangle");
-        } else if (h<a && a<b){
-            return console.log("two triangles (ambiguous case)");
+function ambCase(angle, a, b){
+    //checks if there is a triangle
+    if (angle>0 && a>0 && b>0){
+        const h = b*Math.sin(angle*Math.PI/180);
+        if (angle<=90){
+            if (a==h){
+                return "One triangle";
+            } else if (a>b){
+                return "Right triangle";
+            } else if (h<a && a<b){
+                return "Two triangles (ambiguous case)";
+            }
+        } if (a>b){
+            return "One triangle";
         }
-    } else {
-        if (a<b || a==h){
-            return console.log("no triangle");
-        } else { //check if it is only else
-            return console.log("one triangle");
-        }
-    }
+    } return "No triangle";
 }
 
-function newton(){
-    let g=1; //NEEDS INPUT --> get input
-    let x=g;
+function newton(guess){
+    let x0=guess
+    let x1=x0
+    function fx (x){
+        return 6*Math.pow(x, 4) - 13*Math.pow(x, 3) - 18*Math.pow(x, 2) + 7*x + 6;
+    }
+
+    function fx1 (x){
+        return 24*Math.pow(x, 3) - 39*Math.pow(x, 2) - 36*x + 7
+    }
+
     do {
-        g=x;
-        x=g-(6*Math.pow(g, 4) - 13*Math.pow(g, 3) - 18*Math.pow(g, 2) + 7*g + 6)/(24*Math.pow(g, 3) - 39*Math.pow(g, 2) - 36*g + 7);
-    } while (Math.abs(x-g)>0.0001);
-    console.log(x);
+        x0=x1
+        x1=x0-fx(x0)/fx1(x0);
+    } while (Math.abs(x0-x1)>0.0001);
+    return Math.round(x1*100)/100;
 }
 
 function polynomial(){ //THEORETICAL
